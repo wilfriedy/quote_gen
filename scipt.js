2335fe6c9d0e2afc01cecd69fc05d7dc38785f47
boydeveloper <wilfriedakpe3@gmail.com>
2021-05-17 08:03:36 +0100

    initial commit3

--- scipt.js
+++ scipt.js
@@ -1,18 +1,46 @@
+
 // if (document.readyState == 'ready') {
 //     window.addEventListener('DOMContentLoaded', ready)
 // } else {ready()}
+let quoteText = document.querySelector(".quote-text");
+let authorQuote = document.querySelector('.author')
+let quote = document.querySelector('.inner-box')
+let loader = document.querySelector('.loader')
+let twitter = document.querySelector(".fa-twitter");
+let more  = document.querySelector(".more")
+
+function load() {
+    loader.hidden = false
+    quote.hidden = true
+}
+
+function loadComplete() {
+    if (!loader.hidden) {
+        quote.hidden = false
+        loader.hidden = true
+    }
+}
 
 async function generateQuote() {
-  try {
-    const corsIssues = "https://cors-anywhere.herokuapp.com/";
-    const appUrl =
-      "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
-    const quoteUrl = await fetch(corsIssues + appUrl);
-    const response = await quoteUrl.json();
-    console.log(response);
+    try {
+      load()
+    const quoteUrl = await fetch("https://api.quotable.io/random");
+      const response = await quoteUrl.json();
+      const { author, content } = response
+      quoteText.textContent = content;
+      authorQuote.textContent = author
+      loadComplete()
   } catch (error) {
     console.log("something went wrong " + error);
   }
 }
 
+
 generateQuote();
+
+twitter.addEventListener('click', () => {
+    const tweetTo = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorQuote.innerText} `
+    window.open(tweetTo , '_blank')
+})
+
+more.addEventListener('click', generateQuote)
